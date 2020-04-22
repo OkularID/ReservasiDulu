5101a32c81c4933ed17e64e33663b2334bb32275
MuhammadFikriGunawan <muhammadfikrigunawan@gmail.com>
2020-04-22 09:29:35 +0700

    penambahan template wedding yang ke 3

---
+++ templates/template_wedding_3/app.js
@@ -0,0 +1,45 @@
+// ============Navbar Mobile===============
+  $(document).ready(function() {
+    $('.bars').click(function() {
+      $(".bars").addClass("active");
+      $(".fa-times").addClass("active");
+      $(".sidenav").removeClass("slideOut");
+      $(".sidenav").addClass("slideIn");
+    })
+  })
+  $(document).ready(function() {
+    $('.fa-times').click(function(){
+      $(".bars").removeClass("active");
+      $(".fa-times").removeClass("active");
+      $(".sidenav").addClass("slideOut");
+    })
+  })
+// ============Carousel===============
+$(document).ready(function() {
+  $('.one-carousel').addClass('owl-carousel owl-theme').owlCarousel({
+    margin: 0,
+    responsiveClass: true,
+    loop: true,
+    autoplay: true,
+    responsive:{
+      0: {
+        items: 1,
+        navText: ["<h2 class='text-blue'><i class='fa fa-chevron-circle-left'></i></h2>","<h2 class='text-blue'><i class='fa fa-chevron-circle-right'></i></h2>"],
+        dots: true,
+        nav: false
+      },
+      600: {
+        items: 1,
+        navText: ["<h2 class='text-blue'><i class='fa fa-chevron-circle-left'></i></h2>","<h2 class='text-blue'><i class='fa fa-chevron-circle-right'></i></h2>"],
+        dots: true,
+        nav: false
+      },
+      1000: {
+        items: 1,
+        navText: ["<h2 class='text-blue'><i class='fa fa-chevron-circle-left'></i></h2>","<h2 class='text-blue'><i class='fa fa-chevron-circle-right'></i></h2>"],
+        dots: true,
+        nav: false
+      }
+    }
+  })
+})
