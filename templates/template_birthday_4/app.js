83a6616e2ed48cc5e901e44a0530a9c46ba5806a
hanandito <hananditos@gmail.com>
2020-08-21 14:52:54 +0700

    revisi template

--- templates/template_birthday_4/app.js
+++ templates/template_birthday_4/app.js
@@ -10,17 +10,17 @@ $(document).ready(function() {
     $(".side-nav").removeClass("active");
   })
   // Add smooth scrolling to all links
-  // $("nav a").on('click', function(event) {
-  //   if (this.hash !== "") {
-  //     event.preventDefault();
-  //     var hash = this.hash;
-  //     $('html, body').animate({
-  //       scrollTop: $(hash).offset().top
-  //     }, 1000, function(){
-  //       window.location.hash = hash;
-  //     });
-  //   }
-  // });
+  $(".menu a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1000, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
 });
 // ============Carousel===============
 $(document).ready(function() {
